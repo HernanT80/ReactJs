3903024145da6393d006c0dadb454c6586f5103e
Hernan Torres <hernan.torres278@gmail.com>
2022-03-14 19:31:32 -0300

    creacion del proyecto

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Button from '@mui/material/Button';
+
+function NavBar() {
+    return(
+        <header className = 'main-header'>
+            <div className = 'container-logo'>
+                <img src="logo192.png" className = "img-header" />
+            </div>
+            <ul className = 'navbar'>
+                <li><Button variant="contained">Inicio</Button> </li>
+                <li><Button variant="contained">Productos</Button> </li>
+                <li><Button variant="contained">Nosotros</Button> </li>
+                <li><Button variant="contained">Contacto</Button> </li>
+            </ul>
+        </header>
+    )
+}
+
+export default NavBar;
